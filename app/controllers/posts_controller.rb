2e22437fb6ae89838284207253736801a66448aa
ryoji47 <99850921+ryoji47@users.noreply.github.com>
2022-03-25 19:52:42 +0900

    画像投稿、詳細・削除機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,16 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content, :image)
