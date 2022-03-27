fb4bb2b7da65b85d81b5578a23986f5bd65540d8
ryoji47 <99850921+ryoji47@users.noreply.github.com>
2022-03-27 15:14:01 +0900

    バリデーションの変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :content
+  end
   
+  validates :image, presence: { message: 'を添付してください' }
+
   has_one_attached :image
 end
